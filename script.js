05a52226096f4b872590699b886415ee9f9843b6
himnic23 <105728502+himnic23@users.noreply.github.com>
2025-03-09 22:24:14 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function switchLanguage(lang) {
+  const zhElements = document.querySelectorAll('[id$="-zh"]');
+  const enElements = document.querySelectorAll('[id$="-en"]');
+
+  if (lang === 'zh') {
+    zhElements.forEach(element => element.style.display = 'block');
+    enElements.forEach(element => element.style.display = 'none');
+  } else {
+    zhElements.forEach(element => element.style.display = 'none');
+    enElements.forEach(element => element.style.display = 'block');
+  }
+}
